e9a2d6ae5dd757e016dc377af48ceca3de2b03dc
swapnil0709 <sapnilsinha20@gmail.com>
2023-08-16 11:07:28 +0530

    Code refactor Add check to download dump if not there already Hardcode 14th aug dump url

--- server/const.js
+++ server/const.js
@@ -9,5 +9,11 @@ export const BSE_DUMP_URL = `https://www.bseindia.com/download/BhavCopy/Equity/B
 
 export const NSE_DUMP_URL = `https://archives.nseindia.com/content/historical/EQUITIES/${CURRENT_YEAR}/${getCurrentMonthAbbreviation()}/cm14AUG2023bhav.csv.zip`
 
+export const BSE_DIR_PATH = `./downloads/bse-dump/`
+export const NSE_DIR_PATH = `./downloads/nse-dump/`
+
+export const BSE_FILE_PATH = `./downloads/bse-dump/bse-dump.csv`
+export const NSE_FILE_PATH = `./downloads/nse-dump/nse-dump.csv`
+
 // export const localFilePath = './downloadedFile.txt'
 // console.log({ NSE_DUMP_URL, BSE_DUMP_URL })
